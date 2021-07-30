7255101eb037cc94207a44f915a599612516207e
EUNHEE JANG <6810779s@naver.com>
2021-07-31 00:21:19 +0900

    add

--- specialCafe/detail_js/detail_info.js
+++ specialCafe/detail_js/detail_info.js
@@ -25,6 +25,8 @@ for(let i=0; i<clickCafe.length; i++){
     main.style.display = "none";
     main_detail.style.display = "block";
 
+    console.log(clickCafe[i]);
+    console.log(i);
 
     /*카페 정보*/
     if (clickCafe[i].name==='rooftop'){
@@ -51,6 +53,30 @@ for(let i=0; i<clickCafe.length; i++){
 
       cafeMainTheme_Info.innerHTML = cafeTheme;
       cafeMainName_Info.innerHTML = cafeName;
+    }else if(clickCafe[i].name==='healing'){
+      let cafeTheme = healing[`cafeCategory`];
+      let cafeName = healing[`${clickCafe[i].value}`][`name`];
+
+      cafeTheme_Info.innerHTML = cafeTheme;
+      cafeName_Info.innerHTML = cafeName;
+      cafeAddress_Info.innerHTML = healing[`${clickCafe[i].value}`][`address`];
+      cafeLink_Info.innerHTML = healing[`${clickCafe[i].value}`][`link`];
+      cafeStars_Info.innerHTML = healing[`${clickCafe[i].value}`][`star`];
+
+      cafeMainTheme_Info.innerHTML = cafeTheme;
+      cafeMainName_Info.innerHTML = cafeName;
+    }else if(clickCafe[i].name==='theme'){
+      let cafeTheme = theme[`cafeCategory`];
+      let cafeName = theme[`${clickCafe[i].value}`][`name`];
+
+      cafeTheme_Info.innerHTML = cafeTheme;
+      cafeName_Info.innerHTML = cafeName;
+      cafeAddress_Info.innerHTML = theme[`${clickCafe[i].value}`][`address`];
+      cafeLink_Info.innerHTML = theme[`${clickCafe[i].value}`][`link`];
+      cafeStars_Info.innerHTML = theme[`${clickCafe[i].value}`][`star`];
+
+      cafeMainTheme_Info.innerHTML = cafeTheme;
+      cafeMainName_Info.innerHTML = cafeName;
     }
 
     
@@ -66,63 +92,9 @@ for(let i=0; i<clickCafe.length; i++){
   });
 }
 
+/* 뒤로가기 버튼 */
 
-// clickCafe[0].addEventListener('click',function(){
-//   main.style.display = "none";
-//   main_detail.style.display = "block";
-//   for(let i=0; i<9; i++){
-//     if(i===4){
-//       continue;
-//     }
-//     imgs[i].style.background = `url('../../img/detail_img/rooftop/${clickCafe[0].value}/${i}.png')`;
-//   }
-  
-  // for(let i=0; i<9; i++){
-  //   if(i===4){
-  //     continue;
-  //   }
-  //   console.log(i);
-  //   console.log(clickCafe[0].value);
-  //   imgs[i].style.background = `url('../detail_img/rooftop/regentsky/0.png')`;
-  //   // imgs[i].style.background = `url('../detail_img/rooftop/${clickCafe[0].value}/${i}.png')`;
-  // }
-// });
-
-
-
-/* 별점 넣으려면
-let link__information = {
-  CafeButtonValue1:{link:'link Information', star:'별점 이미지 경로 or 별점 정보:ex 4.3/5'},
-  CafeButtonValue2:{link:'link Information', star:'별점 이미지 경로 or 별점 정보:ex 4.3/5'},
-  CafeButtonValue3:{link:'link Information', star:'별점 이미지 경로 or 별점 정보:ex 4.3/5'},
-
-}
-
-*/
-// clickCafe1.addEventListener('click',function(){
-//   console.log(clickCafe1.value);
-//   imgs[0].style.background = `url('../img/${clickCafe1.value}/logo.png')`;
-// });
-
-
-/*
-
-방법2: 버튼의 value값으로 주기.
-만약 value가 one일때 
-img > one파일을 만듬, img안에 value값과 동일한 이름의 파일들 생성
-파일안에는 해당 value값을 가지고 있는 카페들의 이미지들을 넣음.
-
-for(let i=0; i<9; i++){
-  imgs[i].style.background = `url('../img/${clickCafe.value}/${clickCafe.value}${i}.png')`;
-}
-
-위에짠 코드와 같이 버튼 클릭시 해당 value값을 받아와서 해당 파일을 가져오는 형식.
-
-
-
-information도 위와 같은 방식으로 집어 넣지만 블로그 링크 같은 경우는,
-hashMap을 통해 키와 value값으로 정보를 넣어줘야됨. key에는 버튼읜 value값을,
-value에는 링크정보를 넣어준다.
-
-
-*/ 
+prev.addEventListener('click', function(){
+    main.style.display = "block";
+    main_detail.style.display = "noen";
+});
