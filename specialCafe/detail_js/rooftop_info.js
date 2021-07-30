88536789ef1d2f50c3de4ce5932ef11a9162658c
EUNHEE JANG <6810779s@naver.com>
2021-07-30 23:45:42 +0900

    add

--- specialCafe/detail_js/rooftop_info.js
+++ specialCafe/detail_js/rooftop_info.js
@@ -14,35 +14,35 @@ const rooftop = {
     name: '카페루프탑',
     address: '도로명: 부산 해운대구 청사포로 139-4 4층 카페루프탑',
     link: 'https://blog.naver.com/bse1514/222419401346',
-    star: '4.53/5',
+    star: '평점: 4.53/5',
   },
   
   glasscafe :{
     name: '유리카페',
     address: '도로명 : 인천 강화군 길상면 해안남로 334 2층 유리카페',
     link: 'https://blog.naver.com/1134sin/222366365828',
-    star: '4.51/5',
+    star: '평점: 4.51/5',
   },
   
    karinyoungdoplace :{
     name: '카린 영도 플레이스',
     address: '도로명 : 부산 영도구 청학동로 16',
     link: 'https://blog.naver.com/yoanna735/222371293566',
-    star: '4.45/5',
+    star: '평점: 4.45/5',
   },
   
   lefran :{
     name: '르프랑루프탑',
     address: '도로명 : 서울 종로구 인사동길 34-1 인사아트프라자 5층+6층',
     link: 'https://blog.naver.com/gabrielacsy/222385655777',
-    star: '4.38/5',
+    star: '평점: 4.38/5',
   },
   
   lepicnic :{
     name: '르피크닉',
     address: '도로명 : 제주 서귀포시 안덕면 대평로 39',
     link: 'https://blog.naver.com/hyun1127/222409509112',
-    star: '4.58/5',
+    star: '평점: 4.58/5',
   },
   
   regentsky :{
@@ -56,14 +56,14 @@ const rooftop = {
     name: '루프탑347 카페모든사람들',
     address: '도로명 : 경기 부천시 송내대로42번길 35',
     link: 'https://blog.naver.com/rilri0215/222360470753',
-    star: '4.59/5',
+    star: '평점: 4.59/5',
   },
   
   rooftop357 :{
     name: 'ROOFTOP357',
     address: '도로명 : 경기 남양주시 경강로 358 2층 ROOFTOP357',
     link: 'https://blog.naver.com/2apple/221543540516',
-    star: '4.29/5',
+    star: '평점: 4.29/5',
   },
   
 
