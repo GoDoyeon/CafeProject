45bd3f579cd57d7d393c19a4be73047d7d18600f
EUNHEE JANG <6810779s@naver.com>
2021-07-30 17:59:13 +0900

    refactor

--- specialCafe/special.js/hoverMenu.js
+++ specialCafe/special.js/hoverMenu.js
@@ -2,27 +2,25 @@ let menu = document.querySelectorAll('.cafeKinds li a');
 let underline = document.querySelector('.content__menu .wrap .underline');
 let cnt = 0;
 
-menu[0].addEventListener('mouseover',function(){
-  underline.style.left=0;
-});
+for (let i=0;i<menu.length;i++){
+  if(menu[i].classList.contains('visited')){
+    cnt = i;
+    console.log(cnt);
+    underline.style.left=(cnt*22) + "rem";
+    menu[cnt].style.color = "#8d91eb";
+    break;
+  }
+}
 
-menu[1].addEventListener('mouseover',function(){
-  underline.style.left=22 + "rem";
-});
+for (let i=0;i<menu.length;i++){
+  menu[i].addEventListener('mouseover', function(){
+    underline.style.left = (i*22) + "rem";
+  });
+  menu[i].addEventListener('mouseout', function(){
+    underline.style.left = (cnt*22) + "rem";
+  });
+}
 
-menu[2].addEventListener('mouseover',function(){
-  underline.style.left=44+"rem";
-});
 
-menu[3].addEventListener('mouseover',function(){
-  underline.style.left=66+"rem";
-});
 
-// for (let i=0;i<menu.length;i++){
-//   if(menu[i].classList.contains('visited'));
-//   cnt = i;
-//   break;
-// }
 
-// underline.style.left=(cnt*22) + "rem";
-// document.querySelector(`${menu[cnt]} a`).style.color = "#8d91eb";
