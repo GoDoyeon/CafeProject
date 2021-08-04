0a7d0c5339954073c3eefbd7d1c96e7685448969
EUNHEE JANG <6810779s@naver.com>
2021-08-04 12:04:38 +0900

    detail dark mode

--- darkMode_special/object/detailInfo.js
+++ darkMode_special/object/detailInfo.js
@@ -1,4 +1,4 @@
-const mainHome = {
+const mainHome_detail = {
   setBgColor :function(color){
     let main_home = document.querySelector('.main_detail_info .main__home');
     main_home.style.background = color;
@@ -7,5 +7,27 @@ const mainHome = {
   setFontColor :function(color){
     let mainHome_font = document.querySelector('.main_detail_info .main__home .detailed__information');
     mainHome_font.style.color = color;
-  }
+  },
+
+  setContainerColor : function(color){
+    let container = document.querySelector('.detailed__info');
+    container.style.background = color;
+  },
+
+  setLinkColor: function(color){
+    let link = document.querySelector('.caption a');
+    link.style.color=color;
+  },
+
+  setCenterBgColor: function(color){
+    let img5 = document.querySelector('.detailed__container .img5');
+    img5.style.background=color;
+  },
+
+  setCenterFontColor: function(color){
+    let img5 = document.querySelector('.detailed__container .img5');
+    img5.style.color=color;
+  },
 }
+
+export default mainHome_detail;
