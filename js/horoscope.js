6b5e0c241c27211b1e5fe70a70cf840462be0e5e
GoDoyeon <97godo@naver.com>
2021-08-05 21:51:12 +0900

    메인 페이지 에러 수정
    -[별점 높은 순] 버튼 클릭 시 에러 수정
    -[별점 높은 순] 마우스 호버 시 글씨 정렬 수정

--- js/horoscope.js
+++ js/horoscope.js
@@ -5,32 +5,15 @@ const horoscope = document.querySelector('.horoscope__wrapper');
 let index = 0;
 
 horoPrevBtn.addEventListener('click', () => {
-  console.log('index : ' + index);
-  if (index === 0) {
-    console.log('prev끝');
-    return;
-  }
   index -= 1;
-  horoscope.style.transform = `translate3d(-${1228 * index}px, 0, 0)`;
+  if (index < 4 && index >= 0) {
+    horoscope.style.transform = `translate3d(-${950 * index}px, 0, 0)`;
+  }
 });
 
 horoNextBtn.addEventListener('click', () => {
-  console.log('index : ' + index);
-  console.log('next');
-  if (index === 2) {
-    horoNextBtn.style = 'visibility:hidden;';
-
-    console.log('next끝');
-    return;
-  }
   index += 1;
-  horoscope.style.transform = `translate3d(-${1228 * index}px, 0, 0)`;
+  if (index >= 0 && index < 4) {
+    horoscope.style.transform = `translate3d(-${950 * index}px, 0, 0)`;
+  }
 });
-
-// if (index === 0) {
-//   horoPrevBtn.style = 'visibility:hidden;';
-//   horoNextBtn.style = 'visibility:visible;';
-// } else if (index === 1) {
-//   horoNextBtn.style = 'visibility:hidden;';
-//   horoPrevBtn.style = 'visibility:visible;';
-// }
