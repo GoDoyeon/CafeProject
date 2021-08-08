16c5571dcf7c54ffe399e73703fa6cd89039ab9c
EUNHEE JANG <6810779s@naver.com>
2021-08-08 10:33:04 +0900

    refactor

--- js/horoscope.js
+++ js/horoscope.js
@@ -2,18 +2,38 @@ const horoPrevBtn = document.querySelector('.horoscope__prevbtn');
 const horoNextBtn = document.querySelector('.horoscope__nextbtn');
 const horoscope = document.querySelector('.horoscope__wrapper');
 
+const screen_tablet = window.matchMedia("screen and (max_width: 1023px)");
+const screen_desktop = window.matchMedia("screen and (min-width: 1024px)");
+
 let index = 0;
 
+window.onresize = function(){
+  document.location.reload();
+};
+
 horoPrevBtn.addEventListener('click', () => {
-  index -= 1;
-  if (index < 4 && index >= 0) {
+  if (index <= 4 && index >= 0) {
+    index -= 1;
     horoscope.style.transform = `translate3d(-${950 * index}px, 0, 0)`;
+    if(index===-1){
+      index = 0; 
+    }
   }
+  console.log(`index: ${index}`);
 });
 
 horoNextBtn.addEventListener('click', () => {
-  index += 1;
-  if (index >= 0 && index < 4) {
+  if (index >= 0 && index < 4) {//614
+    index += 1;
     horoscope.style.transform = `translate3d(-${950 * index}px, 0, 0)`;
   }
+  console.log(`index: ${index}`);
 });
+
+if(matchMedia("screen and (max-width: 767px)").matches){
+  console.log("mobile");
+}else if(matchMedia("screen and (max-width: 1023px)").matches){
+  console.log("tablet");
+}else if(matchMedia("screen and (min-width: 1024px)").matches){
+  console.log("desktop");
+}
