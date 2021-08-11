65f166143ac63936bfd7a2dba7cdd46277c4c250
EUNHEE JANG <6810779s@naver.com>
2021-08-11 19:45:44 +0900

    add

--- darkMode_main/object/main_newCafe_color.js
+++ darkMode_main/object/main_newCafe_color.js
@@ -1,9 +1,15 @@
-let mainNewCafe = document.querySelector('.main__newCafe');
-
 const main_newCafe = {
   cafeBgColor : function(color){
+    let mainNewCafe = document.querySelector('.main__newCafe');
     mainNewCafe.style.background = color;
   },
+
+  cafeBtnColor : function(color){
+    let swiper_btn = document.querySelectorAll('.newCafe__block .swiper_button');
+    for(let i=0; i<swiper_btn.length; i++){
+      swiper_btn[i].style.color = color;
+    }
+  }
 }
 
 export default main_newCafe;
