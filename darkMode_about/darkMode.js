136084aa2a7950f69b20f7a231d2201b5f76ff09
EUNHEE JANG <6810779s@naver.com>
2021-08-13 11:30:48 +0900

    add

--- darkMode_about/darkMode.js
+++ darkMode_about/darkMode.js
@@ -40,7 +40,7 @@ darkModeBtn.addEventListener("mouseout", function () {
 function nightDayHandler() {
   let currentPos = document.documentElement.scrollTop;
   let mainHomeHeight = document.querySelector(".main__home").clientHeight;
-  let button_value = document.querySelector(".header__btn .header__btn--right");
+  let button_value = document.querySelector(".header__btn .header__btn--right"); //버튼 글씨
 
   if (darkModeBtn.value === "night") {
     button_value.innerHTML = "라이트모드";
